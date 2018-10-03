8c92966d2234b76ec72b63c4127a7540dd7c4eaf
Ahmed Kamel <Ahmed.Kamel@linkdev.com>
2018-10-03 13:28:48 +0200

    update logs class, add newtensoft json seralizer

--- ApartmentsAllocationHelper/Models/Logger.cs
+++ ApartmentsAllocationHelper/Models/Logger.cs
@@ -1,33 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
 using ApartmentsAllocationHelper.Models.EntityModels;
+using Newtonsoft.Json;
 
 namespace ApartmentsAllocationHelper
 {
     public static class Logger
     {
-        static ApartmentDeliveryDbContext _dbContext; 
+        static ApartmentDeliveryDbContext _dbContext;
+        private static Logs _obj;
+
         public static void WriteLog(String logMessage, string className)
         {
             try
             {
                 using (_dbContext = new ApartmentDeliveryDbContext())
                 {
-                    _dbContext.Logs.Add(new Logs()
+                    _obj = new Logs()
                     {
                         Id = Guid.NewGuid().ToString(),
                         ClassName = className,
                         LogDateTime = DateTime.Now,
                         LogDetails = logMessage
-                    });
+                    };
+                    _dbContext.Logs.Add(_obj);
                     _dbContext.SaveChanges();
                 }
             }
-            catch (Exception) {
-                
+            catch (Exception)
+            {
+                WriteLogToFile(JsonConvert.SerializeObject(_obj));
+            }
+        }
+        private static void WriteLogToFile(string ErrorMessage)
+        {
+            string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string path = (System.IO.Path.GetDirectoryName(executable)) + @"LogsFolder\Logs.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(ErrorMessage);
             }
         }
     }
